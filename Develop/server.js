987c65f9d41745c811ef9cef55ae41388dd060e1
Cynthia <cynthia@thegracetalks.com>
2021-06-25 15:12:27 -0500

    adding content to server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+
+app.listen(PORT, () => {
+    console.log('API server now on port ${PORT}!');
+});
